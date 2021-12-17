9eec8c3104c407179a0e024ad8ace175f86ec279
jfloreshe <jfloreshe@unsa.edu.pe>
2021-12-17 11:33:09 -0500

    Point

--- Point.h
+++ Point.h
@@ -1,11 +1,21 @@
 #ifndef POINT_H
 #define POINT_H
 
-#define MAX_DIM 4 // RGBA
+#define MAX_DIM 3 // RGBA
 
 struct Point {
-    double position[MAX_DIM];
-    int maxDim;      
+    double values[MAX_DIM];
+    int id;
+    int size;
+
+    Point& operator+=(const Point &other) {
+        this->id = -1;
+        this->size = other.size;
+        for(int i = 0; i < this->size; i++){
+            this->values[i] += other.values[i];
+        }
+        return *this;
+    };
 };
 
 #endif
