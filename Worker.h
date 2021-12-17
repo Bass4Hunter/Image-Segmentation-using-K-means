791a91abe55fd0c0eb560293b57087e439a45f47
Gabriel <gabrielito_xvr@hotmail.com>
2021-12-17 12:17:44 -0500

    finished

--- Worker.h
+++ Worker.h
@@ -1,19 +1,67 @@
 #ifndef WORKER_H
 #define WORKER_H
+
+#include <string>
+#include <mpi.h>
+#include <math.h>
+#include <vector>
+#include "Point.h"
+
 class Worker{
 
-    private:
-        int rank;
-        void allReduceNewCentroids();
+private:
+    int rank;
+    MPI_Comm comm;
+    MPI_Datatype pointType;
 
-    public:
-        Node(int rank);
-        ~Node();
+    int total_values;
+    int num_local_points;
+    int K, max_iterations;
+    int numPoints;  
+    int notChanged; 
+
+    int* memCounter;        
+    int lastIteration;
+    bool newDatasetCreated;
+    std::string newDatasetFilename;
+    std::string imgFilename;
+
+    double* reduceArr;
+    double* reduceResults;
+
+    std::vector<Point> dataset;
+    std::vector<Point> localDataset;
+    std::vector<Point> clusters;
+    std::vector<Point> localSum;
+    int numPointsPerNode;
+    std::vector<int> memberships; 
+                           
+    int* globalMembership;
+    double total_time;
+    double omp_total_time;
 
 
-        void scatterPoints(); //  each worker should have n points ( n = totalPoints / totalWorkers)
-        void broadcastCentroids();
-        void nextIteration();
+    int getIdNearestCluster(Point p); 
+    void updateLocalSum(); 
+
+public:
+    Worker(int rank, MPI_Comm comm = MPI_COMM_WORLD);
+    ~Worker();
+    int getMaxIterations();
+    void readDataset();
+    void createDataset(std::string fileImg);
+    void scatterDataset();
+    void extractCluster();
+    int run(int it);
+    void computeGlobalMembership();
+    int getNumPoints();
+    int* getGlobalMemberships();
+    void writeClusterMembership();
+    double squared_norm(Point p1, Point p2);
+    double cosine_similarity(Point p1, Point p2);
+    void setLastIteration(int lastIt);
 
 };
-#endif 
+
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,28 +1,68 @@
 #include <iostream>
-#include <mpi.h>
+#include <vector>
+#include <math.h>
+#include <algorithm>
+#include "Point.h"
 #include "Worker.h"
+#include <stddef.h>
+#include <mpi.h>
+#include <fstream>
+#include <sstream>
+#include "jpeg.h"
 
-#define MAX_ITE 1000
-
+using namespace std;
 int main(int argc, char *argv[]) {
-    int numWokers, rank;
-    
-    
+
+    srand(time(NULL));
+
+    int numNodes, rank;
+    const int tag = 13;
+
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &numWorkers);
+    MPI_Comm_size(MPI_COMM_WORLD, &numNodes);
 
-    Worker worker(rank);
+    int total_values;
+    int total_points;
+    int K, max_iterations;
+    int lastIteration;
+    vector<Point> dataset;
+
+    Worker node(rank, MPI_COMM_WORLD);
+
+    string fileImg = "data/test.jpg";
+    node.createDataset(fileImg);
+    node.readDataset();
+
+    node.scatterDataset();
+    node.extractCluster();
+    lastIteration = 0;
+    for (int it = 0; it < node.getMaxIterations(); it++) {
+
+        int notChanged = node.run(it);
+
+        if(notChanged == numNodes){
+
+            lastIteration = it;
+            
+            break;
+        }
+
+        lastIteration = it;
+    }
+
+    node.setLastIteration(lastIteration);
+
+    node.computeGlobalMembership();
     
-    worker.scatterPoints(); //  each worker should have n points ( n = totalPoints / totalWorkers)
-    worker.broadcastCentroids();
-
-    for(int i=0;i< MAX_ITE;i++){
-        worker.nextIteration();
+    if(rank == 0){
+        int* gm;
+        gm = node.getGlobalMemberships();
+        int numPoints = node.getNumPoints();
+        node.writeClusterMembership();        
     }
 
 
     MPI_Finalize();
 
-    return 0;
 }
